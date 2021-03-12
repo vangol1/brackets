c6d5397d8c0ab9f897522d91aefc0dba81d499e5
Ivan Drozdovskiy <vania110297@gmail.com>
2021-03-12 18:49:44 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.map(el => el.join(''));
+  for (let i = 0; i < brackets.length; i++){
+    if (str.includes(brackets[i])){
+      str = str.replace(brackets[i],"");
+      i = -1;
+    }
+  }
+  if(str)
+    return false;
+  else
+    return true;
 }
